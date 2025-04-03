53314e3e9f9af45359768acc075bbed518bbcf5d
Hoang Tri Minh Tam <hoangtriminhtam@gmail.com>
2025-04-03 18:30:51 +0900

    Renamed a few classes to match the project's integrity

--- src/main/App.java
+++ src/main/App.java
@@ -3,9 +3,10 @@ package main;
 import gui.MainWindow;
 import utils.FontHelper;
 
+@SuppressWarnings("unused")
 public class App {
 	public static void main(String[] args) {
-		FontHelper.printFonts();
+//		FontHelper.printFonts();
 		MainWindow mainWindow = new MainWindow();
 		mainWindow.setVisible(true);
 	}
