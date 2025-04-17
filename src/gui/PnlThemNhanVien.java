0faf53975309738d7f408b5f0ff0e7f9d89ef594
Hoang Tri Minh Tam <hoangtriminhtam@gmail.com>
2025-04-18 01:56:36 +0900

    Resize some stuff

--- src/gui/PnlThemNhanVien.java
+++ src/gui/PnlThemNhanVien.java
@@ -3,6 +3,7 @@ package gui;
 import utils.SwingHelper;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import javax.swing.border.TitledBorder;
 import java.awt.*;
 import java.io.Serial;
@@ -22,13 +23,14 @@ public class PnlThemNhanVien extends JPanel {
         add(lblTitle, BorderLayout.NORTH);
 
         JPanel pnlMain = new JPanel(new GridLayout(1, 2, 20, 0));
-        pnlMain.setBorder(BorderFactory.createEmptyBorder(0, 20, 0, 20));
+        pnlMain.setBorder(BorderFactory.createEmptyBorder(50, 20, 0, 20));
 
         JPanel pnlForm = new JPanel();
         pnlForm.setLayout(new BoxLayout(pnlForm, BoxLayout.Y_AXIS));
         pnlForm.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(),
                 "Form nhân viên", TitledBorder.LEFT, TitledBorder.TOP,
-                new Font("Segoe UI", Font.BOLD, 16)));
+                new Font("Segoe UI", Font.BOLD, 20))
+        );
 
         JLabel lblMaNV = SwingHelper.createProjectJLabel("Mã nhân viên: ");
         JLabel lblTenNV = SwingHelper.createProjectJLabel("Tên nhân viên: ");
@@ -67,13 +69,13 @@ public class PnlThemNhanVien extends JPanel {
         JPanel pnlFields = new JPanel(new GridBagLayout());
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.fill = GridBagConstraints.HORIZONTAL;
-        gbc.insets = new Insets(5, 5, 5, 5);
+        gbc.insets = new Insets(10, 5, 10, 5);
         gbc.weightx = 1.0;
 
         gbc.gridx = 0;
         gbc.gridy = 0;
         gbc.gridwidth = 2;
-        JLabel lblThongTin = SwingHelper.createProjectJLabel("Thông tin nhân viên");
+        JLabel lblThongTin = SwingHelper.createProjectJLabel("Thông tin nhân viên", 20);
         lblThongTin.setHorizontalAlignment(SwingConstants.CENTER);
         pnlFields.add(lblThongTin, gbc);
 
@@ -117,10 +119,9 @@ public class PnlThemNhanVien extends JPanel {
         gbc.gridy = 7;
         gbc.gridx = 0;
         gbc.gridwidth = 2;
-        JLabel lblTaiKhoan = SwingHelper.createProjectJLabel("Tài khoản nhân viên");
+        JLabel lblTaiKhoan = SwingHelper.createProjectJLabel("Tài khoản nhân viên", 20);
         lblTaiKhoan.setHorizontalAlignment(SwingConstants.CENTER);
         pnlFields.add(lblTaiKhoan, gbc);
-
 
         gbc.gridy = 8;
         gbc.gridwidth = 1;
@@ -141,15 +142,16 @@ public class PnlThemNhanVien extends JPanel {
         JPanel pnlRight = new JPanel(new BorderLayout());
         JLabel lblImage = new JLabel();
         lblImage.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-        lblImage.setPreferredSize(new Dimension(375, 500));
+        lblImage.setPreferredSize(new Dimension(350, 480));
 
         JPanel pnlImageHolder = new JPanel(new FlowLayout(FlowLayout.CENTER));
         pnlImageHolder.add(lblImage);
         pnlRight.add(pnlImageHolder, BorderLayout.CENTER);
-        pnlMain.add(pnlForm);
+            pnlMain.add(pnlForm);
         pnlMain.add(pnlRight);
 
         JPanel pnlButtons = new JPanel(new GridLayout(1, 2));
+        pnlButtons.setBorder(new EmptyBorder(0, 0,50, 0));
         JPanel pnlLeftButtons = new JPanel(new FlowLayout(FlowLayout.CENTER));
         JButton btnLamMoi = new JButton("Làm mới");
         JButton btnThem = new JButton("Thêm");
