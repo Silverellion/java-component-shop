0375d7185722e8b1c0661a827f3496a143322da4
Hoang Tri Minh Tam <hoangtriminhtam@gmail.com>
2025-04-20 17:33:26 +0900

    Move SwingSidebarHelper's classes to WindowMain_GUI

--- src/gui/WindowMain_GUI.java
+++ src/gui/WindowMain_GUI.java
@@ -6,13 +6,15 @@ import gui.Kho.PnlNhapHang;
 import gui.Kho.PnlThongKeKho;
 import gui.NhanVien.PnlCapNhatNhanVien;
 import gui.NhanVien.PnlThemNhanVien;
-import utils.SwingSidebarHelper;
 
 import javax.swing.*;
+import javax.swing.plaf.basic.BasicMenuItemUI;
 import java.awt.*;
 import java.awt.event.*;
 import java.io.Serial;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
 public class WindowMain_GUI extends JFrame implements ActionListener {
     @Serial
@@ -38,6 +40,8 @@ public class WindowMain_GUI extends JFrame implements ActionListener {
     PnlCapNhatNhanVien pnlCapNhatNhanVien;
     PnlTrangChu pnlTrangChu;
     PnlCaiDat pnlCaiDat;
+
+    private final Map<JButton, JPopupMenu> buttonMenuMap = new HashMap<>();
 
     public WindowMain_GUI() {
         setTitle("Cửa hàng linh kiện");
@@ -91,14 +95,14 @@ public class WindowMain_GUI extends JFrame implements ActionListener {
         btnDangNhap.addActionListener(this);
         btnDangXuat.addActionListener(this);
 
-        SwingSidebarHelper.registerDropdown(btnQuanLyKhoPanel, "Nhập hàng", pnlNhapHang, pnlEast, this::resetSidebarColors);
-        SwingSidebarHelper.registerDropdown(btnQuanLyKhoPanel, "Thống kê kho", pnlThongKeKho, pnlEast, this::resetSidebarColors);
+        registerDropdown(btnQuanLyKhoPanel, "Nhập hàng", pnlNhapHang, pnlEast, this::resetSidebarColors);
+        registerDropdown(btnQuanLyKhoPanel, "Thống kê kho", pnlThongKeKho, pnlEast, this::resetSidebarColors);
 
-        SwingSidebarHelper.registerDropdown(btnDonHangPnl, "Tạo đơn hàng", pnlTaoDonHang, pnlEast, this::resetSidebarColors);
-        SwingSidebarHelper.registerDropdown(btnDonHangPnl, "Thống kê đơn hàng", pnlThongKeDonHang, pnlEast, this::resetSidebarColors);
+        registerDropdown(btnDonHangPnl, "Tạo đơn hàng", pnlTaoDonHang, pnlEast, this::resetSidebarColors);
+        registerDropdown(btnDonHangPnl, "Thống kê đơn hàng", pnlThongKeDonHang, pnlEast, this::resetSidebarColors);
 
-        SwingSidebarHelper.registerDropdown(btnQuanLyNhanVienPnl, "Thêm nhân viên", pnlThemNhanVien, pnlEast, this::resetSidebarColors);
-        SwingSidebarHelper.registerDropdown(btnQuanLyNhanVienPnl, "Cập nhật nhân viên", pnlCapNhatNhanVien, pnlEast, this::resetSidebarColors);
+        registerDropdown(btnQuanLyNhanVienPnl, "Thêm nhân viên", pnlThemNhanVien, pnlEast, this::resetSidebarColors);
+        registerDropdown(btnQuanLyNhanVienPnl, "Cập nhật nhân viên", pnlCapNhatNhanVien, pnlEast, this::resetSidebarColors);
     }
 
     private JPanel createPanel(String title) {
@@ -193,5 +197,97 @@ public class WindowMain_GUI extends JFrame implements ActionListener {
         btnDonHangPnl.setBackground(new Color(200, 60, 60));
         btnQuanLyNhanVienPnl.setBackground(new Color(200, 60, 60));
         btnCaiDatPanel.setBackground(new Color(200, 60, 60));
+    }
+
+    private void registerDropdown(JButton button, String itemName, JPanel panel, JPanel pnlMain, Runnable backgroundResetter) {
+        JPopupMenu menu = buttonMenuMap.computeIfAbsent(button, btn -> {
+            JPopupMenu newMenu = new JPopupMenu();
+            newMenu.setBorder(BorderFactory.createLineBorder(Color.GRAY));
+            newMenu.setBackground(new Color(200, 60, 60));
+            newMenu.setFocusable(false);
+
+            btn.addMouseListener(new MouseAdapter() {
+                Timer hoverTimer;
+
+                @Override
+                public void mouseEntered(MouseEvent e) {
+                    hoverTimer = new Timer(100, u ->  newMenu.show(btn, btn.getWidth(), 0));
+                    hoverTimer.setRepeats(false);
+                    hoverTimer.start();
+                }
+
+                @Override
+                public void mouseExited(MouseEvent e) {
+                    if (hoverTimer != null && hoverTimer.isRunning()) {
+                        hoverTimer.stop();
+                    }
+
+                    PointerInfo pi = MouseInfo.getPointerInfo();
+                    Point location = pi.getLocation();
+                    SwingUtilities.convertPointFromScreen(location, newMenu);
+
+                    if (!newMenu.getBounds().contains(location)) {
+                        newMenu.setVisible(false);
+                    }
+                }
+            });
+
+            return newMenu;
+        });
+
+        JMenuItem menuItem = new JMenuItem(itemName);
+        styleMenuItem(menuItem);
+
+        menuItem.addActionListener(e -> {
+            backgroundResetter.run();
+            button.setBackground(new Color(200, 0, 0));
+            pnlMain.removeAll();
+            pnlMain.add(panel);
+
+            // Refresh data if switching to X panel that needs to sync with any given dynamic array
+            if (panel instanceof PnlCapNhatNhanVien) {
+                ((PnlCapNhatNhanVien) panel).refreshData();
+            }
+
+            pnlMain.revalidate();
+            pnlMain.repaint();
+            menu.setVisible(false);
+        });
+
+        menu.add(menuItem);
+    }
+
+    private void styleMenuItem(JMenuItem menuItem) {
+        menuItem.setFont(new Font("Segoe UI", Font.BOLD, 16));
+        menuItem.setOpaque(true);
+        menuItem.setBackground(new Color(200, 60, 60));
+        menuItem.setForeground(Color.WHITE);
+        menuItem.setPreferredSize(new Dimension(200, 50));
+        menuItem.setBorder(BorderFactory.createEmptyBorder(5, 10, 5, 10));
+        menuItem.setFocusPainted(false);
+        menuItem.setContentAreaFilled(false);
+        menuItem.setBorderPainted(false);
+        menuItem.setUI(new BasicMenuItemUI() {
+            @Override
+            protected void installDefaults() {
+                super.installDefaults();
+                selectionBackground = new Color(200, 0, 0);
+                selectionForeground = Color.WHITE;
+            }
+        });
+
+        menuItem.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                menuItem.setBackground(new Color(200, 0, 0));
+                menuItem.setForeground(Color.WHITE);
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                menuItem.setBackground(new Color(200, 60, 60));
+                menuItem.setForeground(Color.WHITE);
+            }
+        });
     }
 }
